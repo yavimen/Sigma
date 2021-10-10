03060cfc6fcfee38ad48121556b402b5af1b4056
yavimen <91732561+yavimen@users.noreply.github.com>
2021-10-10 16:31:29 +0300

    Add files via upload

---
+++ Polynomial/Polynomial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PolynomialTask.Classes;
+
+namespace PolynomialTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polynomial p1 = new Polynomial();
+            p1.Parse("3+1*x^1+3*x^2");
+            Polynomial p2 = new Polynomial();
+            p2.Parse("2+1*x^2");
+
+            Console.WriteLine(p1*p2);
+        }
+    }
+}
