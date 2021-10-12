16d2bc439c0cff9ddf3e5a9a012ffb274ee89534
yavimen <91732561+yavimen@users.noreply.github.com>
2021-10-12 18:31:13 +0300

    Add files via upload

---
+++ Task3/ElectricityAccounting/MyReadFromFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace SigmaTask3.Classes
+{
+    class MyReadFromFile
+    {
+        public static void ReadFile(ref int apartmentsAmount, ref Quarter quarter, ref Apartment[] apartments, string filepath)
+        {
+            StreamReader file = new StreamReader(filepath);
+            if (file == null)
+                throw new FileNotFoundException("Path is wrong!");
+
+
+            string templine = file.ReadLine();
+
+            string[] selements = templine.Split(" ");
+
+            apartmentsAmount = int.Parse(selements[0]);
+
+            apartments = new Apartment[apartmentsAmount];
+
+            quarter = (Quarter)Enum.GetValues(typeof(Quarter)).GetValue(int.Parse(selements[1]) - 1);
+
+            for (int i = 0; i < apartmentsAmount; ++i)
+            {
+                templine = file.ReadLine();
+                selements = templine.Split(" ");
+                apartments[i] = new Apartment(int.Parse(selements[0]), selements[1], int.Parse(selements[2]), int.Parse(selements[3]),
+                    int.Parse(selements[4]), int.Parse(selements[5]), int.Parse(selements[6]), int.Parse(selements[7]));
+            }
+        }
+    }
+}
---
+++ Task3/MagicSquare/MagicSquare.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigmaMagicSquare
+{
+    class MagicSquare
+    {
+        int nsize;
+
+        public MagicSquare(int size) 
+        {
+            nsize = size;
+        }
+
+        private void CreateRomb(ref int[,] rmatrix)
+        {
+            int counter = 1;
+            for (int i = 0; i < nsize; ++i)
+            {
+                for (int j = 0; j < nsize; j++)
+                {
+                    rmatrix[nsize - 1 - j + i, j + i] = counter++;
+                }
+            }
+            return;
+        }
+
+        public int[,] CreateMagicSquare() 
+        {
+            int fSize = nsize + nsize - 1;
+            int[,] rmatrix = new int[fSize, fSize];
+            CreateRomb(ref rmatrix);
+
+
+            for (int i = 0; i <= (nsize - 1) / 2; i++)
+            {
+                for (int j = 0; j < rmatrix.GetLength(1); ++j)
+                {
+                    if (rmatrix[i, j] != 0)
+                    {
+                        rmatrix[i + nsize, j] = rmatrix[i, j];
+                        rmatrix[i, j] = 0;
+                    }
+                }
+            }
+
+            for (int i = rmatrix.GetLength(0) - 1; i >= rmatrix.GetLength(0) - (nsize - 1) / 2; i--)
+            {
+                for (int j = 0; j < rmatrix.GetLength(1); ++j)
+                {
+                    if (rmatrix[i, j] != 0)
+                    {
+                        rmatrix[i - nsize, j] = rmatrix[i, j];
+                        rmatrix[i, j] = 0;
+                    }
+                }
+            }
+
+            for (int j = 0; j < (nsize - 1) / 2; j++)
+            {
+                for (int i = 0; i < rmatrix.GetLength(1); i++)
+                {
+                    if (rmatrix[i, j] != 0)
+                    {
+                        rmatrix[i, j + nsize] = rmatrix[i, j];
+                        rmatrix[i, j] = 0;
+                    }
+                }
+            }
+            for (int j = rmatrix.GetLength(0) - 1; j >= rmatrix.GetLength(0) - (nsize - 1) / 2; j--)
+            {
+                for (int i = 0; i < rmatrix.GetLength(1); i++)
+                {
+                    if (rmatrix[i, j] != 0)
+                    {
+                        rmatrix[i, j - nsize] = rmatrix[i, j];
+                        rmatrix[i, j] = 0;
+                    }
+                }
+            }
+
+            int[,] magicSquare = new int[nsize, nsize];
+            for (int i = 0; i < nsize; i++)
+            {
+                for (int j = 0; j < nsize; j++)
+                {
+                    magicSquare[i, j] = rmatrix[(nsize - 1) / 2 + i, (nsize - 1) / 2 + j];
+                }
+            }
+            return magicSquare;
+        }
+    }
+}
