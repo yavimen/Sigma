cd137ba6f7f5e4adc1f1858456259f8beb22a893
yavimen <91732561+yavimen@users.noreply.github.com>
2021-10-12 18:36:38 +0300

    Add files via upload

---
+++ Task5/3.FilePath.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StartingCSharp
+{
+    class FilePath
+    {
+        private string path;
+
+        public string Path 
+        {
+            get { return Path; }
+        }
+
+        public FilePath(string filepath) 
+        {
+            this.path = filepath;
+        }
+
+        public string GetFileName() 
+        {
+            string []temp = path.Split('\\',' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] namewithextension = temp[temp.Length - 1].Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            if (namewithextension.Length > 2)
+                throw new Exception("Wrong filename!");
+            return namewithextension[0];
+        }
+
+        public string GetRootFolderName() 
+        {
+            string[] temp = path.Split('\\', ' ', StringSplitOptions.RemoveEmptyEntries);
+            return temp[temp.Length-2];
+        }
+    }
+}
