3ebe40de83ce311d41a97d6ec073ff0c7fdce063
yavimen <91732561+yavimen@users.noreply.github.com>
2021-11-08 23:59:35 +0200

    Add files via upload

---
+++ Task11/GiftParts/BoysGreetings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Sigma_SaintNicholas
+{
+    class BoysGreetings
+    {
+        static BoysGreetings _instance;
+
+        private List<string> _ngreetings;
+
+        private BoysGreetings() => MakeGreetingsFromFile();
+
+        static public BoysGreetings GetInstance() 
+        {
+            if (_instance == null)
+            {
+                _instance = new BoysGreetings();
+            }
+            return _instance;
+        }
+
+        public string this[int index]
+        {
+            get { 
+                if(_ngreetings.Count > index)
+                    return _ngreetings[index];
+                throw new Exception("Index out of range!");
+            }
+        }
+
+        public int Lenght { get { return _ngreetings.Count; } }
+
+        public void MakeGreetingsFromFile() 
+        {
+            string ftext = File.ReadAllText("./BoysGreetings.txt");
+            _ngreetings = new List<string>(ftext.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public void Add(string greeting) 
+        {
+            _ngreetings.Add(greeting);
+        }
+
+        public void Delete(string greeting) 
+        {
+            _ngreetings.Remove(greeting);
+        }
+    }
+}
---
+++ Task11/GiftParts/GirlsGreetings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Sigma_SaintNicholas
+{
+    class GirlsGreetings
+    {
+        static GirlsGreetings _instance;
+
+        private List<string> _ngreetings;
+
+        private GirlsGreetings() => MakeGreetingsFromFile();
+
+        static public GirlsGreetings GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new GirlsGreetings();
+            }
+            return _instance;
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (_ngreetings.Count > index)
+                    return _ngreetings[index];
+                throw new Exception("Index out of range!");
+            }
+        }
+
+        public int Lenght { get { return _ngreetings.Count; } }
+
+        public void MakeGreetingsFromFile()
+        {
+            string ftext = File.ReadAllText("./GirlsGreetings.txt");
+            _ngreetings = new List<string>(ftext.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public void Add(string greeting)
+        {
+            _ngreetings.Add(greeting);
+        }
+
+        public void Delete(string greeting)
+        {
+            _ngreetings.Remove(greeting);
+        }
+    }
+}
