0b0d71d3f0b9e5d481adc6e845d12e22f4d8048d
yavimen <91732561+yavimen@users.noreply.github.com>
2021-11-21 17:00:57 +0200

    Add files via upload

---
+++ Task12/SigmaShop_V2.0/SigmaShop_V2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿using SigmaShop.StoragesSystem;
+using SigmaShop.PromoSystem;
+using SigmaShop.OrderSystem;
+using SigmaShop.Users;
+using SigmaShop.ShopSystem;
+
+namespace SigmaShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CStoreSystem storeSystem = new CStoreSystem(CStorageBase.GetInstance(), CPromoBase.GetInstance(), 
+                CAuthUserBase.GetInstance(), COrderBase.GetInstance(), CShop.GetInstance(), new CUIBridge());
+
+            storeSystem.ShowMainPage();
+        }
+    }
+}
